8bb918e6801957cfc76023ca799d9ce5b8b9b399
akupemaludeh <102456463+akupemaludeh@users.noreply.github.com>
2022-04-01 12:14:03 +0700

    Update warmachine.sh

--- warmachine.sh
+++ warmachine.sh
@@ -2,3 +2,8 @@ wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/dan
 tar -xvf danila-miner-2.3.1-ubuntu-bionic.tar.gz
 mv danila-miner dnila
 chmod +x dnila
+
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.47/lolMiner_v1.47_Lin64.tar.gz
+tar -xvf lolMiner_v1.47_Lin64.tar.gz
+mv /content/1.4.7/lolminer /content/elol
+chmod +x elol
