e225fd0342f3883d341b822452d519d209121296
akupemaludeh <102456463+akupemaludeh@users.noreply.github.com>
2022-04-01 11:49:30 +0700

    Create warmachine.sh

---
+++ warmachine.sh
@@ -0,0 +1,4 @@
+wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-ubuntu-bionic.tar.gz
+tar -xvf danila-miner-2.3.1-ubuntu-bionic.tar.gz
+mv danila-miner dnila
+chmod +x dnila
